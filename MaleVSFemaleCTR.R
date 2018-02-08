37509dfd8fd610ef1c777053680c62c4ff6ef505
Trent Callan <trentcallan@Trents-MacBook-Pro-2.local>
2018-02-08 13:24:34 -0800

    Updated Trent's files

--- MaleVSFemaleCTR.R
+++ MaleVSFemaleCTR.R
@@ -1,4 +1,11 @@
+#GETTING DATA FOR 31 DAYS FOR FEMALES
+#data1.frame = female
 data1.frame <- data.frame(day = 0, CTR = 0)
+
+charts <- function(temp, day1) {
+  CTRfemale <- subset(temp, Gender==0)$Clicks/subset(temp, Gender==0)$Impressions
+  data1.frame <- rbind(data1.frame, c(day1, mean(CTRfemale, na.rm = TRUE)))
+}
 
 for (day in c(1:31)){
   tempCSV <- paste("HW1_data/nyt",day, ".csv", sep="")
@@ -7,24 +14,20 @@ for (day in c(1:31)){
   data1.frame <- charts(nyt, day)
 }
 
-charts <- function(temp, day1) {
-  CTRfemale <- subset(temp, Gender==1)$Clicks/subset(temp, Gender==1)$Impressions
-  data1.frame <- rbind(data1.frame, c(day1, mean(CTRfemale, na.rm = TRUE)))
-  
-}
-#female
-#print(data1.frame)
 data1.frame <- data1.frame[-c(33),]
 data1.frame <- data1.frame[-c(1),]
-#sd(data1.frame$CTR)
-#string1 <- paste("Female mean CTR:", mean(data1.frame$CTR))
-#print(string1)
 barplot(data1.frame$CTR, names.arg=days, xlab="Days", ylab="CTR", main="CTR Female", ylim=c(0,0.025))
 
 
-#male
+#GETTING DATA FOR 31 DAYS FOR MALE
+#data2.frame = male
 data2.frame <- data.frame(day = 0, CTR = 0)
 days <- c(1:31)
+
+charts2 <- function(temp, day1) {
+  CTRmale <- subset(temp, Gender==1)$Clicks/subset(temp, Gender==1)$Impressions
+  data2.frame <- rbind(data2.frame, c(day1, mean(CTRmale, na.rm = TRUE)))
+}
 
 for (day in c(1:31)){
   tempCSV <- paste("HW1_data/nyt",day, ".csv", sep="")
@@ -33,28 +36,20 @@ for (day in c(1:31)){
   data2.frame <- charts2(nyt, day)
 }
 
-charts2 <- function(temp, day1) {
-  CTRmale <- subset(temp, Gender==0)$Clicks/subset(temp, Gender==0)$Impressions
-  data2.frame <- rbind(data2.frame, c(day1, mean(CTRmale, na.rm = TRUE)))
-  
-}
 
-#print(data2.frame)
+#ALL THE PLOTS ARE BELOW
 data2.frame <- data2.frame[-c(1),]
-#sd(data2.frame$CTR)
-#string2 <- paste("Male mean CTR:", mean(data2.frame$CTR))
-#print(string2)
 barplot(data2.frame$CTR, names.arg=days, xlab="Days", ylab="CTR", main="CTR Male", ylim=c(0,0.025))
 
-#female
+#female summary
 summary(data1.frame$CTR)
-#male
+#male summary
 summary(data2.frame$CTR)
 
-#density plot of both male and female CTR
-plot(density(data1.frame$CTR), col="pink", main="CTR Density Male versus Female", xlim=c(0.013,0.025))
-lines(density(data2.frame$CTR), col="blue")
-legend("topright", c("Male", "Female"), cex=1, fill=c("blue", "pink"))
+#density plot of both male and female CTR together
+#plot(density(data1.frame$CTR), col="pink", main="CTR Density Male versus Female", xlim=c(0.013,0.025))
+#lines(density(data2.frame$CTR), col="blue")
+#legend("topright", c("Male", "Female"), cex=1, fill=c("blue", "pink"))
 
 #male and female density plots individual
 plot(density(data1.frame$CTR), col="pink", main="CTR Density Female", xlab="CTR")
--- Signed_inVSSigned_outCTR.R
+++ Signed_inVSSigned_outCTR.R
@@ -1,4 +1,10 @@
+#SIGNED IN
 Signed_in <- data.frame(day = 0, CTR = 0)
+
+charts <- function(data1, month1) {
+  CTRSigned_in <- subset(data1, Signed_In==1)$Clicks/subset(data1, Signed_In==1)$Impressions
+  Signed_in <- rbind(data1.frame, c(month1, mean(CTRSigned_in, na.rm = TRUE)))
+}
 
 for (month in c(1:31)){
   temp <- paste("HW1_data/nyt",month, ".csv", sep="")
@@ -7,25 +13,19 @@ for (month in c(1:31)){
   Signed_in <- charts(nyt2, month)
 }
 
-charts <- function(data1, month1) {
-  CTRSigned_in <- subset(data1, Signed_In==1)$Clicks/subset(data1, Signed_In==1)$Impressions
-  Signed_in <- rbind(data1.frame, c(month1, mean(CTRSigned_in, na.rm = TRUE)))
-  
-}
-
-#signed in
-#print(Signed_in)
 Signed_in <- Signed_in[-c(32),]
-#Signed_in <- Signed_in[-c(1),]
-#sd(Signed_in$CTR)
-#string2 <- paste("Signed in mean CTR:", mean(Signed_in$CTR))
-#print(string2)
 barplot(Signed_in$CTR, names.arg=days, xlab="Days", ylab="CTR", main="CTR Signed in", ylim=c(0,0.03))
 
 
-#signed out
+
+#SIGNED OUT
 Signed_out <- data.frame(day = 0, CTR = 0)
 days <- c(1:31)
+
+charts2 <- function(temp, day1) {
+  CTRSigned_out <- subset(temp, Signed_In==0)$Clicks/subset(temp, Signed_In==0)$Impressions
+  Signed_out <- rbind(Signed_out, c(day1, mean(CTRSigned_out, na.rm = TRUE)))
+}
 
 for (day in c(1:31)){
   tempCSV <- paste("HW1_data/nyt",day, ".csv", sep="")
@@ -34,35 +34,27 @@ for (day in c(1:31)){
   Signed_out <- charts2(nyt, day)
 }
 
-charts2 <- function(temp, day1) {
-  CTRSigned_out <- subset(temp, Signed_In==0)$Clicks/subset(temp, Signed_In==0)$Impressions
-  Signed_out <- rbind(Signed_out, c(day1, mean(CTRSigned_out, na.rm = TRUE)))
-  
-}
-
 Signed_out <- Signed_out[-c(1),]
-#print(Signed_out)
-#sd(Signed_out$CTR)
-#string2 <- paste("Signed out mean CTR:", mean(Signed_out$CTR))
-#print(string2)
 barplot(Signed_out$CTR, names.arg=days, xlab="Days", ylab="CTR", main="CTR Signed out", ylim=c(0,0.03))
 
 #signed in
-summary(Signed_in$CTR)
+#summary(Signed_in$CTR)
 #signed out
-summary(Signed_out$CTR)
+#summary(Signed_out$CTR)
+
+#PLOTS ARE BELOW
 
 #density plot of both male and signed in CTR
-plot(density(Signed_in$CTR), col="purple", main="Signed in and Male CTR", xlim=c(0.017,0.025), ylim=c(0,450), xlab="CTR")
-lines(density(data2.frame$CTR), col="red")
-legend("topleft", c("Signed in", "Male"), cex=0.7, fill=c("purple", "red"))
+plot(density(Signed_in$CTR), col="purple", main="Signed in and Female CTR", xlim=c(0.017,0.025), ylim=c(0,450), xlab="CTR")
+lines(density(data1.frame$CTR), col="red")
+legend("topleft", c("Signed in", "Female"), cex=0.7, fill=c("purple", "red"))
 
-#signed in vs singed out
+#signed in and singed out density plots
 plot(density(Signed_in$CTR), col="brown", main="CTR Density Signed in", xlab="CTR")
-sd(Signed_in$CTR)
+#sd(Signed_in$CTR)
 plot(density(Signed_out$CTR), col="red", main="CTR Density Signed out", xlab="CTR")
-sd(Signed_out$CTR)
+#sd(Signed_out$CTR)
 
-#scatter plots of days vs signed in CTR and male CTR
+#scatter plots of days vs signed in CTR and female CTR
 plot(Signed_in$day, Signed_in$CTR, xlab="Day", ylab="CTR", main="Signed in CTR")
-plot(data2.frame$day, data2.frame$CTR, xlab="Day", ylab="CTR", main="Male CTR")
+plot(data2.frame$day, data1.frame$CTR, xlab="Day", ylab="CTR", main="Female CTR")
